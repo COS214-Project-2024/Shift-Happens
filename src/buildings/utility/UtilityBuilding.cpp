3b1b177f3e65ec39b7508d3d32b80d9c7be49067
MornevanHeerden <u21482153@tuks.co.za>
2024-10-27 15:53:27 +0200

    Quick commit

--- src/buildings/utility/UtilityBuilding.cpp
+++ src/buildings/utility/UtilityBuilding.cpp
@@ -1,5 +1,11 @@
 #include "UtilityBuilding.h"
 
+UtilityBuilding::UtilityBuilding(int id,string variant, int constructionPrice, int wasteProduced, int sewageProduced, int electricityDemand, int waterDemand)
+: Building(id,1, 1, -1, -1, variant, "Utility",constructionPrice,wasteProduced,sewageProduced,electricityDemand,waterDemand)
+{
+	this->runningCostPerHour = 0;
+}
+
 int UtilityBuilding::getRunningCostPerHour() {
 	return this->runningCostPerHour;
 }
--- src/buildings/utility/UtilityBuilding.h
+++ src/buildings/utility/UtilityBuilding.h
@@ -2,9 +2,13 @@
 #define UTILITYBUILDING_H
 
 #include "../Building.h"
+
 class UtilityBuilding : public Building {
 
 	public:
+
+		UtilityBuilding(int id,string variant, int constructionPrice, int wasteProduced, int sewageProduced, int electricityDemand, int waterDemand);
+
 		int runningCostPerHour;
 
 		int getRunningCostPerHour();
