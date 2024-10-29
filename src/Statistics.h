f67292e519bfae5c159ebacbfe652f27b7eb4233
brogrammer012 <nigelmofati@gmail.com>
2024-10-29 03:32:17 +0200

    updated statistics class

--- src/Statistics.h
+++ src/Statistics.h
@@ -13,18 +13,17 @@
 	class Statistics {
 	private:
 		std::vector<Building*> buildings; //track all building in the game
-		double citySatisfactionScore;
-		double employmentRate;
-		
+		double citySatisfactionScore; //this score will be assigned by the getAverageCitizenSatisfaction function
+		double employmentRate; //this score will be assigned by the getEmploymentRates function
+
 	public:
 		Statistics(std::string description, double cost, int quantity);
-		void addBuilding(Building *building);
-		void removeBuilding(Building *building);
-		void getBuilding(int id);
-		void render();
+		void addBuilding(Building *building); //this needs to work with the constructors for all the buildings
+		void removeBuilding(Building *building); //this needs to work with the destructors for all the buildings
 		double getAverageCitizenSatisfaction(Citizen* average);
 		void getEmploymentRates(Government* var); //should calculate the employment rates for the city and assign to employment rate
 		void displayStatistics(); //should call the displays for all buildings in the model
+		// void getBuilding(int id); // function necessity to be determined
 	};
 
 
