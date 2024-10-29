f2d041e20044ac9e6f4ff98a81fe5cc716347d56
herrie732 <herrie732@gmail.com>
2024-10-29 14:53:21 +0200

    Updated

---
+++ src/Government/BoostHealthCarePolicy.h
@@ -0,0 +1,19 @@
+#ifndef BOOSTHEALTHCAREPOLICY
+#define BOOSTHEALTHCAREPOLICY
+
+#include "Policy.h"
+#include "HealthCare.h"
+// this policy has a cost, if the budget is enough then it will take education to the next level and build one more education centre or upgrade and existing one
+// invoker is the government
+// this policy 
+// receiver is education
+
+class BoostHealthCarePolicy : public Policy {
+  private:
+    double Cost = 1000;
+    HealthCare* healthcare;
+  public:
+    void executePolicy(double AvailableBudget);
+};
+
+#endif
