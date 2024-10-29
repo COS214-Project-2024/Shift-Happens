54e99c00863cfa8c4517267b74a38a19ff870140
brogrammer012 <nigelmofati@gmail.com>
2024-10-29 03:24:36 +0200

    defined the statistics class

--- src/Statistics.h
+++ src/Statistics.h
@@ -1,9 +1,31 @@
 #ifndef STATISTICS_H
 #define STATISTICS_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Materials.h"
+#include "Building.h"
+#include "Citizen.h"
+#include "Government.h"
 
-namespace Building_State {
+
+
 	class Statistics {
+	private:
+		std::vector<Building*> buildings; //track all building in the game
+		double citySatisfactionScore;
+		double employmentRate;
+		
+	public:
+		Statistics(std::string description, double cost, int quantity);
+		void addBuilding(Building *building);
+		void removeBuilding(Building *building);
+		void getBuilding(int id);
+		void render();
+		double getAverageCitizenSatisfaction(Citizen* average);
+		void getEmploymentRates(Government* var); //should calculate the employment rates for the city and assign to employment rate
+		void displayStatistics(); //should call the displays for all buildings in the model
 	};
-}
+
 
 #endif
