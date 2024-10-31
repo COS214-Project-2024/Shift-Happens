9b2e87ff4d596209e0c1ec5d0afe94a6b4e8a326
herrie732 <herrie732@gmail.com>
2024-10-31 19:46:34 +0200

    Added unit test for citizen folder

--- src/unit_testing/CitizenUnitTesting.cpp
+++ src/unit_testing/CitizenUnitTesting.cpp
@@ -6,6 +6,12 @@
 #include "../citizens/Citizen.h"
 #include "../citizens/BoyBuilder.h"
 #include "../citizens/Boy.h"
+#include "../citizens/Girl.h"
+#include "../citizens/GirlBuilder.h"
+#include "../citizens/Woman.h"
+#include "../citizens/WomanBuilder.h"
+#include "../citizens/Man.h"
+#include "../citizens/ManBuilder.h"
 
 
 /*
@@ -17,6 +23,7 @@ TEST(PolicyTest, CanCreateEducationService) {
 }
 */
 
+// Boy builder tests
 TEST(BoyBuilderTest, addGenderTest){
   std::shared_ptr<BoyBuilder> boybuilder = make_shared<BoyBuilder>();
   boybuilder->addGender();
@@ -34,20 +41,153 @@ TEST(BoyBuilderTest, addTypeTest){
 }
 
 TEST(BoyBuilderTest, addStatusTestWithValidInput) {
-    std::shared_ptr<BoyBuilder> boyBuilder = std::make_shared<BoyBuilder>();
+  std::shared_ptr<BoyBuilder> boyBuilder = std::make_shared<BoyBuilder>();
 
-    // Simulate "Yes" input
-    std::istringstream input("Yes\n");
-    std::cin.rdbuf(input.rdbuf());  // Redirect std::cin
+  std::istringstream input("Yes\n");
+  std::cin.rdbuf(input.rdbuf());
 
-    boyBuilder->addStatus();
-    std::shared_ptr<Citizen> citizen = boyBuilder->getCitizen();
-    ASSERT_NE(citizen, nullptr);
-
-    std::shared_ptr<Boy> boy = std::dynamic_pointer_cast<Boy>(citizen);
-    ASSERT_NE(boy, nullptr);
-    EXPECT_TRUE(boy->getStatus());  // Expect the boy to be in school
+  boyBuilder->addStatus();
+  std::shared_ptr<Citizen> boy = boyBuilder->getCitizen();
+  ASSERT_NE(boy, nullptr);
+  EXPECT_TRUE(boy->getStatus()); 
 }
+
+TEST(BoyBuilderTest, addStatusTestWithInvalidInput) {
+  std::shared_ptr<BoyBuilder> boyBuilder = std::make_shared<BoyBuilder>();
+
+  std::istringstream input("Maybe\nno\n");
+  std::cin.rdbuf(input.rdbuf()); 
+
+  boyBuilder->addStatus();
+  std::shared_ptr<Citizen> boy = boyBuilder->getCitizen();
+  ASSERT_NE(boy, nullptr);
+  EXPECT_FALSE(boy->getStatus());
+}
+
+// Girl Builder tests
+TEST(GirlBuilderTest, addGenderTest){
+  std::shared_ptr<GirlBuilder> girlBuilder = std::make_shared<GirlBuilder>();
+  girlBuilder->addGender();
+  std::shared_ptr<Citizen> girl = girlBuilder->getCitizen();
+  ASSERT_NE(girl, nullptr);
+  EXPECT_EQ(girl->getGender(), "Female");
+}
+
+TEST(GirlBuilderTest, addTypeTest){
+  std::shared_ptr<GirlBuilder> girlBuilder = std::make_shared<GirlBuilder>();
+  girlBuilder->addType();
+  std::shared_ptr<Citizen> girl = girlBuilder->getCitizen();
+  ASSERT_NE(girl, nullptr);
+  EXPECT_EQ(girl->getType(), "Girl");
+}
+
+TEST(GirlBuilderTest, addStatusTestWithValidInput) {
+  std::shared_ptr<GirlBuilder> girlBuilder = std::make_shared<GirlBuilder>();
+
+  std::istringstream input("Yes\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  girlBuilder->addStatus();
+  std::shared_ptr<Citizen> girl = girlBuilder->getCitizen();
+  ASSERT_NE(girl, nullptr);
+  EXPECT_TRUE(girl->getStatus()); 
+}
+
+TEST(GirlBuilderTest, addStatusTestWithInvalidInput) {
+  std::shared_ptr<GirlBuilder> girlBuilder = std::make_shared<GirlBuilder>();
+
+  std::istringstream input("Maybe\nno\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  girlBuilder->addStatus();
+  std::shared_ptr<Citizen> girl = girlBuilder->getCitizen();
+  ASSERT_NE(girl, nullptr);
+  EXPECT_FALSE(girl->getStatus());
+}
+
+// Woman Builder tests
+TEST(WomanBuilderTest, addGenderTest){
+  std::shared_ptr<WomanBuilder> womanBuilder = std::make_shared<WomanBuilder>();
+  womanBuilder->addGender();
+  std::shared_ptr<Citizen> woman = womanBuilder->getCitizen();
+  ASSERT_NE(woman, nullptr);
+  EXPECT_EQ(woman->getGender(), "Female");
+}
+
+TEST(WomanBuilderTest, addTypeTest){
+  std::shared_ptr<WomanBuilder> womanBuilder = std::make_shared<WomanBuilder>();
+  womanBuilder->addType();
+  std::shared_ptr<Citizen> woman = womanBuilder->getCitizen();
+  ASSERT_NE(woman, nullptr);
+  EXPECT_EQ(woman->getType(), "Woman");
+}
+
+TEST(WomanBuilderTest, addStatusTestWithValidInput) {
+  std::shared_ptr<WomanBuilder> womanBuilder = std::make_shared<WomanBuilder>();
+
+  std::istringstream input("Yes\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  womanBuilder->addStatus();
+  std::shared_ptr<Citizen> woman = womanBuilder->getCitizen();
+  ASSERT_NE(woman, nullptr);
+  EXPECT_TRUE(woman->getStatus()); 
+}
+
+TEST(WomanBuilderTest, addStatusTestWithInvalidInput) {
+  std::shared_ptr<WomanBuilder> womanBuilder = std::make_shared<WomanBuilder>();
+
+  std::istringstream input("Maybe\nno\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  womanBuilder->addStatus();
+  std::shared_ptr<Citizen> woman = womanBuilder->getCitizen();
+  ASSERT_NE(woman, nullptr);
+  EXPECT_FALSE(woman->getStatus());
+}
+
+// Man Builder tests
+TEST(ManBuilderTest, addGenderTest){
+  std::shared_ptr<ManBuilder> manBuilder = std::make_shared<ManBuilder>();
+  manBuilder->addGender();
+  std::shared_ptr<Citizen> man = manBuilder->getCitizen();
+  ASSERT_NE(man, nullptr);
+  EXPECT_EQ(man->getGender(), "Male");
+}
+
+TEST(ManBuilderTest, addTypeTest){
+  std::shared_ptr<ManBuilder> manBuilder = std::make_shared<ManBuilder>();
+  manBuilder->addType();
+  std::shared_ptr<Citizen> man = manBuilder->getCitizen();
+  ASSERT_NE(man, nullptr);
+  EXPECT_EQ(man->getType(), "Man");
+}
+
+TEST(ManBuilderTest, addStatusTestWithValidInput) {
+  std::shared_ptr<ManBuilder> manBuilder = std::make_shared<ManBuilder>();
+
+  std::istringstream input("Yes\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  manBuilder->addStatus();
+  std::shared_ptr<Citizen> man = manBuilder->getCitizen();
+  ASSERT_NE(man, nullptr);
+  EXPECT_TRUE(man->getStatus()); 
+}
+
+TEST(ManBuilderTest, addStatusTestWithInvalidInput) {
+  std::shared_ptr<ManBuilder> manBuilder = std::make_shared<ManBuilder>();
+
+  std::istringstream input("Maybe\nno\n");
+  std::cin.rdbuf(input.rdbuf());
+
+  manBuilder->addStatus();
+  std::shared_ptr<Citizen> man = manBuilder->getCitizen();
+  ASSERT_NE(man, nullptr);
+  EXPECT_FALSE(man->getStatus());
+}
+
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
