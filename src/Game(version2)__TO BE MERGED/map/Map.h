4eefa30e525eb609dc468595bcd17a4afb9f1cfc
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-31 23:49:06 +0200

    Update Map.h

--- src/Game(version2)__TO BE MERGED/map/Map.h
+++ src/Game(version2)__TO BE MERGED/map/Map.h
@@ -3,30 +3,45 @@
 
 #include <string>
 #include <vector>
+//########################
+// Define color codes
+//########################
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define BLUE "\033[34m"
+#define ORANGE "\033[38;5;208m"
+#define CYAN "\033[36m"
+
+
 
 using namespace std;
 
 class Map
 {
 private:
-    vector<vector<string>> tiles;
+    vector<vector<string>> tiles;   //components
     int width, height;
 
 public:
-    Map(int width = 25, int height = 25);
+    Map(int width, int height);
 
     bool add(const string& component, int xCoord, int yCoord);
 
     bool remove(int xCoord, int yCoord);
+
+    bool change(string newName, int xCoord, int yCoord);
 
     string getComponent(int xCoord, int yCoord) const;
 
     void render() const;
 
     //##
-    vector<vector<string>> getComponents();
+    vector<vector<string>> getComponents(); //return the tiles/components in map
+    vector<pair<string,string>> upgraded_tiles;  //upgraded components
     int getWidth();
     int getHeight();
+    int getNumComponents();
 };
 
 #endif
