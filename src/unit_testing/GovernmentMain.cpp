3f325a537118b5357694a5bf7dddf6d711b88816
Ntokozo254 <u22506773@tuks.co.za>
2024-10-31 14:56:57 +0200

    Add unit tests and update Makefile for GovernmentMain, This way you can run different mains with their specified unit testing components separately using makefile commands.

---
+++ src/unit_testing/GovernmentMain.cpp
@@ -0,0 +1,81 @@
+#include <memory>
+#include <string>
+#include <iostream>
+#include <gtest/gtest.h>
+
+
+//Game
+#include "../game/Game.h"
+
+//BuildingFactories
+#include "../buildingFactories/ResidentialBuildingFactory.h"
+#include "../buildingFactories/CommercialBuildingFactory.h"
+#include "../buildingFactories/IndustrialBuildingFactory.h"
+#include "../buildingFactories/UtilityBuildingFactory.h"
+#include "../buildingFactories/LandmarkBuildingFactory.h"
+#include "../buildingFactories/InfrastructureFactory.h"
+#include "../buildingFactories/TransportBuildingFactory.h" // Include TransportBuildingFactory
+
+#include "../buildingFactories/BuildingFactory.h"
+
+//Buildings
+#include "../buildings/Building.h"
+
+#include "../buildings/residential/House.h"
+#include "../buildings/residential/TownHouse.h"
+#include "../buildings/residential/Estate.h"
+#include "../buildings/residential/Apartment.h"
+
+#include "../buildings/commercial/Store.h"
+#include "../buildings/commercial/Office.h"
+#include "../buildings/commercial/Mall.h"
+
+#include "../buildings/industrial/Factory.h"
+#include "../buildings/industrial/Warehouse.h"
+#include "../buildings/industrial/Manufacturer.h"
+
+#include "../buildings/utility/PowerPlant.h"
+#include "../buildings/utility/WaterSupply.h"
+#include "../buildings/utility/SewagePlant.h"
+#include "../buildings/utility/LandFill.h"
+
+#include "../buildings/landmark/Monument.h"
+#include "../buildings/landmark/Park.h"
+#include "../buildings/landmark/CulturalCenter.h"
+
+#include "../buildings/infrastructure/Road.h"
+#include "../buildings/infrastructure/Railway.h"
+
+#include "../buildings/transport/Airport.h"
+#include "../buildings/transport/BusStation.h"
+#include "../buildings/transport/TrainStation.h"
+
+
+//Policies
+#include "../Government/PublicServiceState.h"
+#include "../Government/Standard.h"
+#include "../Government/Outdated.h"
+#include "../Government/Modern.h"
+#include "../Government/PublicServices.h"
+#include "../Government/CitySubject.h"
+#include "../citizens/CitizenObserver.h"
+#include "../Government/Education.h"
+#include "../Government/HealthCare.h"
+#include "../Government/Police.h"
+#include "../Government/BoostEducationPolicy.h"
+#include "../Government/BoostHealthCarePolicy.h"
+#include "../Government/BoostPolicePolicy.h"
+#include "../Government/Policy.h"
+
+using namespace std;
+
+
+TEST(ExampleTest, SimpleTest) { //mock test to check if Gtest is working
+    EXPECT_EQ(1 + 1, 2);
+}
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
