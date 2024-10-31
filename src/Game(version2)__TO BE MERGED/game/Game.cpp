2e4f207c7ba7043d2c4094a0a9fa1c9a2a3a0a88
Samkelekile Ndevu <81513440+samkelekile-ndevu@users.noreply.github.com>
2024-10-31 23:45:29 +0200

    Update Game.cpp

--- src/Game(version2)__TO BE MERGED/game/Game.cpp
+++ src/Game(version2)__TO BE MERGED/game/Game.cpp
@@ -29,7 +29,7 @@ void Game::run()
 {
     display.intro();
     MainMenu();
-    map->render();
+    showMap();
     buldingmenu();
     // display.wait(6000);
 }
@@ -69,7 +69,7 @@ void Game::newGame()
 {
     display.clear();
     display.logo();
-    
+
     // map->render();
 
     std::cout << "Enter your name: " << std::endl;
@@ -82,18 +82,18 @@ void Game::newGame()
     display.wait(1);
 
     std::cout << "Generating map.." << std::endl;
-    map = new Map(25, 25); // 25 by 25
+    map = new Map(15, 15); // [15 X 15]
 
-    display.wait(1);
-    std::cout << "Setting up construction..." << std::endl;
+    // display.wait(1);
+    // std::cout << "Setting up construction..." << std::endl;
+    // display.wait(1);
+    // std::cout << "Getting people to move in..." << std::endl;
 
-    display.wait(1);
-    std::cout << "Getting people to move in..." << std::endl;
-
-    display.wait(1);
-    std::cout << "Establishing a government..." << std::endl;
+    // display.wait(1);
+    // std::cout << "Establishing a government..." << std::endl;
 
     display.loadscreen();
+    display.clear();
 
     display.wait(1);
     // std::cout << endl;
@@ -109,8 +109,6 @@ void Game::underConstruction()
 {
     display.clear();
     display.logo();
-    
-    map->render();
 
     std::cout << "Under Construction..." << std::endl;
     display.wait(2);
@@ -122,7 +120,7 @@ void Game::underConstruction()
 
 void Game::buldingmenu()
 {
-    
+
     cout << "Lets get started with the simulation!\n";
     int choice = display.GameMenu();
     bool valid = false;
@@ -162,24 +160,32 @@ void Game::buldingmenu()
 
 void Game::showBuildings()
 {
-    std::cout << "Showing you created buildings.\n";
+    std::cout << "\nCreated buildings:\n";
     vector<vector<string>> mapComponents = map->getComponents();
-
-
-    for (int i = 0; i < map->getWidth(); i++) {
-        for (int j = 0; j < map->getHeight(); j++) {
-            if (mapComponents[i][j] != "") {
-               cout << "* " << mapComponents[i][j] << " is located at (" << i << "," << j << ")\n";
+    bool isEmpty = true;
+    for (int i = 0; i < map->getWidth(); i++)
+    {
+        for (int j = 0; j < map->getHeight(); j++)
+        {
+            if (mapComponents[i][j] != "")
+            {
+                isEmpty = false;
+                cout << "* " << BLUE << mapComponents[i][j] << RESET << " is located at (" << BLUE << i << "," << j << ")\n"
+                     << RESET;
             }
         }
     }
+    
+    if(isEmpty == true){
+        system("clear");
+        std::cout << "\n0 buildings... Start creating something it's easy\n";
+    }
 
-    //7 second delay
+    // 7 second delay
     display.wait(7);
 
-    //show menu options
+    // show menu options
     buldingmenu();
-
 }
 
 void Game::createBuidling()
@@ -241,7 +247,6 @@ void Game::createBuidling()
             break;
         }
     }
-
 }
 void Game::createVariant(const int &op)
 {
@@ -278,20 +283,39 @@ void Game::createVariant(const int &op)
 
     // Prompt for coordinates after confirming the building variant
     std::pair<int, int> coordinates = getBuildCoordinates();
-    
-    //use concat(building name, and Variant first letters),
+
+    // use concat(building name, and Variant first letters),
     std::string compName = getVar().substr(0, 1) + display.getvariant().substr(0, 1);
     map->add(compName, coordinates.first, coordinates.second);
-    
-    std::cout << "Building " << compName << " at coordinates (" << coordinates.first << ", " << coordinates.second << ")." << std::endl;
-    
-    //display the map
-    map->render();
-    display.wait(10);   //10 seconds delay
 
-    //show menu
+    std::cout << "Building " << compName << " at coordinates (" << coordinates.first << ", " << coordinates.second << ")." << std::endl;
+
+    // display the map
+    showMap();
+    // display.wait(10);   //10 seconds delay
+
+    // show menu
     buldingmenu();
 }
+
+void Game::showMap()
+{
+    display.clear();
+    display.logo();
+    cout << endl;
+    map->render();
+    cout << "\n " << BLUE << "Press C to proceed" << RESET << endl;
+    string s = "";
+    cin >> s;
+    if (s.length() == 0)
+    {
+        showMap();
+    }
+    else{
+        return;
+    }
+}
+
 void Game::demolishvariant(const int &op)
 {
     bool valid2 = false;
@@ -325,19 +349,19 @@ void Game::demolishvariant(const int &op)
     }
 
     // Prompt for coordinates after confirming the building variant
-    std::pair<int, int> coordinates = getBuildCoordinates();
-    
-    //use concat(building name, and Variant first letters),
-    std::string compName = getVar().substr(0, 1) + display.getvariant().substr(0, 1);
-    map->add(compName, coordinates.first, coordinates.second);
-    
-    std::cout << "Demolished " << compName << " at coordinates (" << coordinates.first << ", " << coordinates.second << ")." << std::endl;
-    
-    //display the map
-    map->render();
-    display.wait(10);   //10 seconds delay
+    std::pair<int, int> coordinates = getDestroyedBuilding();
 
-    //show menu
+    // use concat(building name, and Variant first letters),
+    std::string compName = getVar().substr(0, 1) + display.getvariant().substr(0, 1);
+    map->remove(coordinates.first, coordinates.second);
+
+    std::cout << "Demolished " << compName << " at coordinates (" << coordinates.first << ", " << coordinates.second << ")." << std::endl;
+
+    // display the map
+    showMap();
+    // display.wait(10);   //10 seconds delay
+
+    // show menu
     buldingmenu();
 }
 void Game::upgradevariant(const int &op)
@@ -352,11 +376,11 @@ void Game::upgradevariant(const int &op)
             valid2 = true;
             break;
         case 2:
-            cout << "Now uograding" << getVar() << " buidling: " << display.getvariant() << endl;
+            cout << "Now upgrading" << getVar() << " buidling: " << display.getvariant() << endl;
             valid2 = true;
             break;
         case 3:
-            cout << "Now uograding" << getVar() << "building: " << display.getvariant() << endl;
+            cout << "Now upgrading" << getVar() << "building: " << display.getvariant() << endl;
             valid2 = true;
             break;
         case 4:
@@ -371,6 +395,29 @@ void Game::upgradevariant(const int &op)
             break;
         }
     }
+
+    // Prompt for coordinates after confirming the building variant
+    std::pair<string, std::pair<int, int>> coordinates = getUpgradedBuilding();
+
+    // use concat(building name, and Variant first letters),
+    std::string compName = coordinates.first;
+    map->remove(coordinates.second.first, coordinates.second.second);
+    map->add(compName, coordinates.second.first, coordinates.second.second);
+
+    string coordinatesPair = "(" + std::to_string(coordinates.second.first) + std::to_string(coordinates.second.second) + ")";
+
+    map->upgraded_tiles.emplace_back(coordinates.first, coordinatesPair);
+
+    std::cout << "Upgraded " << compName << " at coordinates (" << coordinates.second.first << ", " << coordinates.second.second << ")." << std::endl;
+
+    display.wait(2);
+    
+    // display the map
+    showMap();
+    // display.wait(10);   //10 seconds delay
+
+    // show menu
+    buldingmenu();
 }
 
 void Game::demolishbuilding()
@@ -503,53 +550,126 @@ bool Game::validatevariant(string &variant)
     return valid;
 }
 
-//#################################################
-//We use these below for building and destroying
-//#################################################
+// #################################################
+// We use these below for building and destroying
+// #################################################
 
-//Builder coordinates
-std::pair<int, int> Game::getBuildCoordinates() {
+// Builder coordinates
+std::pair<int, int> Game::getBuildCoordinates()
+{
     int x, y;
 
-    std::cout << "Enter the coordinates for the new building (x y): ";
-    std::cin >> x >> y;
+    std::cout << "Enter the coordinates for the new building's location";
+    std::cout << "\nEnter the x coordinate:";
+    std::cin >> x;
+
+    std::cout << "\nEnter the y coordinate:";
+    std::cin >> y;
 
     // validate coordinates
-    if(map->getComponent(x, y) != ""){
+    if (map->getComponent(x, y) != "")
+    {
         cout << "Location occupied, choose another location.";
         return getBuildCoordinates();
     }
 
-    //no error, cool
+    // no error, cool
     return std::make_pair(x, y);
 }
 
-//Demolisher coordinates and nane
-std::pair<string, std::pair<int,int>> getDetroyedBuilding() {
-    
+// Demolisher coordinates and nane
+std::pair<int, int> Game::getDestroyedBuilding()
+{
+
     int x, y;
     string buildingName;
 
-    //get location on map
-    std::cout << "Enter the coordinates for the building you want to detroy (x y): ";
-    std::cin >> x >> y;
+    // get location on map
+    std::cout << "Enter the coordinates for the building you want to detroy";
+    std::cout << "\nEnter the x coordinate:";
+    std::cin >> x;
+
+    std::cout << "\nEnter the y coordinate:";
+    std::cin >> y;
 
     // validate coordinates
-    if(map->getComponent(x, y) == ""){
+    if (map->getComponent(x, y) == "")
+    {
         cout << "No building at such location, choose another location.";
-        return getDetroyedBuilding();
+        return getDestroyedBuilding();
     }
 
-    //get name of building or component from map
-    std::cout << "Enter the name for the building you want to detroy: ";
+    // get name of building or component from map
+    // std::cout << "Enter the name for the building you want to detroy: ";
+    // std::cin >> buildingName;
+
+    // // validate name
+    // if (map->getComponent(x, y) != buildingName)
+    // {
+    //     cout << "No building at such location, choose another location.";
+    //     return getDestroyedBuilding();
+    // }
+
+    // no error, cool
+    string name = map->getComponent(x, y);
+    cout << GREEN << name << "Commercial building is being being destroyed." << RESET;
+    
+    return make_pair(x, y);
+}
+
+// Upgrader coordinates
+std::pair<string, std::pair<int, int>> Game::getUpgradedBuilding()
+{
+
+    int x, y;
+    string buildingName;
+
+    // get location on map
+    std::cout << "Enter the coordinates for the building you want to upgrade. ";
+    std::cout << "\nEnter the x coordinate:";
+    std::cin >> x;
+
+    std::cout << "\nEnter the y coordinate:";
+    std::cin >> y;
+
+    // validate coordinates
+    if (map->getComponent(x, y) == "")
+    {
+        cout << "No building at such location, choose another location.";
+        return getUpgradedBuilding();
+    }
+
+    // get name of building or component from map
+    std::cout << "Enter the name for the building you want to upgrade: ";
     std::cin >> buildingName;
 
-    //validate name
-    if(map->getComponent(x, y) != buildingName){
-        cout << "No building at such location, choose another location.";
-        return getDetroyedBuilding();
+    // validate name
+    // if (map->getComponent(x, y) != buildingName)
+    // {
+    //     cout << "No building at such location, choose another location.";
+    //     return getUpgradedBuilding();
+    // }
+
+    // got it, cool!
+    if (buildingName.at(0) == 'C')
+    {
+        cout << GREEN << "Commercial building is being upgraded.";
+    }
+    else if (buildingName.at(0) == 'U')
+    {
+        cout << GREEN << "Utility building is being upgraded.";
+    }
+    else if (buildingName.at(0) == 'I')
+    {
+        cout << GREEN << "Industrial building is being upgraded.";
+    }
+    else if (buildingName.at(0) == 'R')
+    {
+        cout << GREEN << "Residential building is being upgraded.";
     }
 
-    //no error, cool
+    // map->change(buildingName, x, y);
+
+    // no error, cool
     return std::make_pair(buildingName, make_pair(x, y));
 }
