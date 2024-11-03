f879654dde053bde133061ab617272eb94ecb14e
herrie732 <herrie732@gmail.com>
2024-11-04 01:53:22 +0200

    Updates transportation

--- src/map/Transportation/PublicStrategy.cpp
+++ src/map/Transportation/PublicStrategy.cpp
@@ -1,20 +1,10 @@
 #include "PublicStrategy.h"
 #include "Map.h"
 
-PublicStrategy::PublicStrategy() 
-    : capacity(15), 
-      baseFare(15), 
-      costPerKm(10), 
-      aveSpeed(55.0) {}
-
-double PublicStrategy::calculateCost(shared_ptr<Map> map, int startX, int startY, int endX, int endY) {
-    double distance = map->roadDistanceTo(startX, startY, endX, endY);
-	string mssg = "This TAXI trip costs: R";
-    return mssg, baseFare + (costPerKm * distance);
+double PublicStrategy::calculateCost(int distance) {
+    return distance*costPerKm;
 }
 
-double PublicStrategy::calculateDuration(shared_ptr<Map> map, int startX, int startY, int endX, int endY) {
-    double distance = map->roadDistanceTo(startX, startY, endX, endY);
-	string mssg = "The TAXI trip will take: (mins)";
-    return mssg, (distance / aveSpeed) * 60; 
+double PublicStrategy::calculateDuration(int distance) {
+    return distance/this->speed;
 }
