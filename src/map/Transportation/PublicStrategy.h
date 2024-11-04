e1cbcf45d6c5af5112dfee0036366364f3725d74
brogrammer012 <nigelmofati@gmail.com>
2024-11-04 09:56:56 +0200

    added documentation to public strategy

--- src/map/Transportation/PublicStrategy.h
+++ src/map/Transportation/PublicStrategy.h
@@ -7,22 +7,66 @@
 
 using namespace std;
 
-class PublicStrategy : public TransportationStrategy {
+/**
+ * @class PublicStrategy
+ * @brief Implements the public transportation strategy for city simulation.
+ *
+ * Provides methods to calculate transportation cost and duration for public transport, along with attributes such as capacity, speed, and cost per kilometer.
+ */
+class PublicStrategy : public TransportationStrategy
+{
 private:
-    int capacityMin = 5;
-    int capacityMax = 15;
-    double speed = 55;
-    double costPerKm = 10;
+    int capacityMin = 5;   /**< Minimum capacity for public transportation. */
+    int capacityMax = 15;  /**< Maximum capacity for public transportation. */
+    double speed = 55;     /**< Speed of the public transportation in km/h. */
+    double costPerKm = 10; /**< Cost per kilometer for public transportation. */
 
 public:
+    /**
+     * @brief Calculates the cost of public transportation based on the distance.
+     * @param distance The distance to be traveled in kilometers.
+     * @return The cost of the trip.
+     */
     double calculateCost(int distance) override;
+
+    /**
+     * @brief Calculates the duration of the trip based on the distance.
+     * @param distance The distance to be traveled in kilometers.
+     * @return The duration of the trip in hours.
+     */
     double calculateDuration(int distance) override;
+
+    /**
+     * @brief Returns the type of transportation.
+     * @return The type of transportation as a string.
+     */
     string getType() override { return "Taxi"; }
 
-    //getters
+    // Getters
+
+    /**
+     * @brief Gets the minimum capacity for public transportation.
+     * @return The minimum capacity.
+     */
     int getCapacityMin() const;
+
+    /**
+     * @brief Gets the maximum capacity for public transportation.
+     * @return The maximum capacity.
+     */
     int getCapacityMax() const;
+
+    /**
+     * @brief Gets the speed of public transportation.
+     * @return The speed in km/h.
+     */
     double getSpeed() const;
+
+    /**
+     * @brief Gets the cost per kilometer for public transportation.
+     * @return The cost per kilometer.
+     */
     double getCostPerKm() const;
 };
-#endif
+
+#endif // PUBLICSTRATEGY_H
